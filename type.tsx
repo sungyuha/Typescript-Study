2588abd3c61831a43809ddece4a11d65f3074499
YUHASUNG <yuha822@naver.com>
2022-12-13 23:55:01 +0900

    type object

---
+++ type.tsx
@@ -0,0 +1,19 @@
+// 타입 스크립트는 타입을 지정해줘야 함
+let a : number[] = [1,2];
+let b : string[] = ["il","1"];
+let c : boolean[] = [true];
+
+// ex - error code
+/*const player = {
+    // typescript가 player 안에 있는 name은 string이라는 것을 추론함
+    name: "yuri"
+}
+player.name = 1;*/
+
+const player : {
+    name:string,
+    age:number
+} = {
+    name: "yuri",
+    age: 25
+}
