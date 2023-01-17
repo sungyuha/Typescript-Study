72dd5102e4367bae277c8a389e54d9fc0194ab25
YUHASUNG <yuha822@naver.com>
2023-01-17 23:16:56 +0900

    tpyescript 타입추론

--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
-function logName (name: string) {
-    console.log(name);
+let student = {
+    // 타입스크립트는 객체 속의 프로퍼티를 조사하고 어떤 값이 할당 값인지 확인 햄
+    name: 'Jack',
+    course: 'Gettin Started with Typscript',
+    codingIQ: 80,
+    code: function () {
+        console.log('brain is working hard');
+    }
 }
-
-logName('Jack');
