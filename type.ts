aca21db398a79f6cde00f4403310910793fc877f
YUHASUNG <yuha822@naver.com>
2023-01-20 21:33:15 +0900

    dosc: 타입으로 사용되는 인터페이스:

--- type.ts
+++ type.ts
@@ -5,6 +5,16 @@ let gender:string = 'female';
 let subject:string = 'Javascript';
 let courseCompleted: boolean = false;
 
+// 오브젝트를 바깥에 새성하고 오브젝트의 이름을 assignment 에 패스
+let student1 = {
+    studentID: 122233,
+    studentName: 'Janet Jacksan',
+    age: 30,
+    gender: 'female',
+    subject: 'Mongo DB',
+    courseCompleted: false
+}
+
 // 타입으로 사용되는 인터페이스
 interface Student {
     // 뱐화되는 객체의 구조를 타입으로 지정
@@ -29,3 +39,13 @@ function getStuentDetails(studentID: number): Student
         courseCompleted: true
     };
 }
+
+// 또 다른 함수를 추가, 새롭게 추가 된 함ㅅ는 파라미터로 학생의 정보를 받아서 그 정보를 저장하는 함수
+// 파라미터에 방금 전 선언한 Interface 인 Student 를 4개의 변수에 타입으로 지정 - 코드 재사용
+function saveStuentDetails(student: Student): void
+{
+
+}
+
+// 아무 문제 없이 컴파일
+saveStuentDetails(student1);
