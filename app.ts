f1eee963089a4f33fb3f6e6997eaaba2c69f5dba
YUHASUNG <yuha822@naver.com>
2023-02-01 20:36:19 +0900

    dosc: typescript any type

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-let student = {
+/*let student = {
     // 타입스크립트는 객체 속의 프로퍼티를 조사하고 어떤 값이 할당 값인지 확인 햄
     name: 'Jack',
     course: 'Gettin Started with Typscript',
@@ -6,4 +6,12 @@ let student = {
     code: function () {
         console.log('brain is working hard');
     }
-}
+}*/
+
+// any타입 : 어떠한 타입이든 모든 OK!
+let someValue: any = 5;
+someValue = 'hello';
+someValue = true;
+
+// 타입스크립트는 '타입에 관한 더 많은 정보를 명시할 수록 더 좋음!'
+// 타입을 명확히 하면 컴파일 에러도 잡고 -> 효괴적인 코드의 유지 보수
