cb0d54d4a5d63de15f38c55c0352aed21c76f127
YUHASUNG <yuha822@naver.com>
2023-02-17 00:18:46 +0900

    dosc: Constructor & Access Modifiers

--- Constructor.ts
+++ Constructor.ts
@@ -1,25 +1,39 @@
 // 1. Constructor(생성자) : 클래스 로부터 객체를 생성할 때, 호출되며, "객체의 초기화를 담당"
 class Employeee {
-    private _funName: string; // 클래스 내에서 멤버변수를 나타내는 fullName 앞에서 _ (underscore) 를 넣어줌
+    /*private _funName: string; // 클래스 내에서 멤버변수를 나타내는 fullName 앞에서 _ (underscore) 를 넣어줌
     age: number;
     jobTitle: string;
     hourlyRate: number;
-    workingHoursPerWeek: number;
+    workingHoursPerWeek: number;*/
+
 
     // constructor는 매개변수, 즉 파라미터를 가질 수 있음 
-    constructor(funName:string, age:number, jobTitle:string, // 전달되는 매개변수를 강제하고 있음
-    hourlyRate:number, workingHoursPerWeek: number) {
+    constructor(
+        /*funName:string, 
+        age:number, 
+        jobTitle:string, // 전달되는 매개변수를 강제하고 있음
+        hourlyRate:number, 
+        workingHoursPerWeek: number) { */
     
     // 객체가 생성될 때 반드시 할당하지 않아도 되는 값들로 만들고 싶으면 콘론 앞에 ?를 붙이고 선택적 매개변수로 만들어주면 됨
     /*constructor(funName:string, age:number, jobTitle?:string, // 전달되는 매개변수를 강제하고 있음
     hourlyRate:number, workingHoursPerWeek: number) {*/
 
         // thos 뒤에 나타는 객체는 상단의 변수들을 가리킴
-        this.funName = funName;
+        /*this.funName = funName;
         this.age = age;
         this.jobTitle = jobTitle;
         this.hourlyRate = hourlyRate;
-        this.workingHoursPerWeek = workingHoursPerWeek;
+        this.workingHoursPerWeek = workingHoursPerWeek;*/
+        
+        /* constructor 의 매개변수 앞에 접근 제한자 (Access Modifiers)를 앞에 붙여주면 Access Modifiers가 사용된 파라미터는 암묵적으로 class의 필드 멤버변수로 선언 됨
+            -> 즉, 객체가 생성될 때, 컨스트럭터의 매개변수로 전달 된 값은, 객체의 프로퍼티 값으로 자동으로 그 값이 초기화 되고 할당 됨
+        */
+        private _funName:string, 
+        private _age:number, 
+        private _jobTitle:string,
+        private _hourlyRate:number, 
+        public _workingHoursPerWeek: number) {
     }
 
     // Getter 와 Setter -> Get 과 Set 키워드를 사용하여 Getter 와 Setter 를 선언 
@@ -36,8 +50,11 @@ class Employeee {
 
     printEmployeeDetails = ():void => {
         // this.funName}는 상단에 선언 된 변수들을 가리킴
-        console.log(`${this._funName}의 작업은 ${this.jobTitle} 이고 일주일의 수업은 
-        ${this.hourlyRate * this.workingHoursPerWeek} 달러 이다.`)
+        /*console.log(`${this._funName}의 작업은 ${this.jobTitle} 이고 일주일의 수업은 
+        ${this.hourlyRate * this.workingHoursPerWeek} 달러 이다.`)*/
+
+        console.log(`${this._funName}의 작업은 ${this._jobTitle} 이고 일주일의 수업은 
+        ${this._hourlyRate * this._workingHoursPerWeek} 달러 이다.`)
     }
 }    
 
@@ -74,3 +91,8 @@ let employeee2 = new Employeee('미나', 32, '프로젝트 매니저', 55, 35);
 // 3. Getter 와 Setter
 // 접근 불가능한 private를 접근 가능하게 만들기 위해 사용
 // Get 과 Set 키워드를 사용하여 Getter 와 Setter 를 선언 
+
+// 4.  Constructor 와 Access Modifiers
+/* 타입스크립트에는 코드를 간결하게 나타내는 방법이 존재함
+    -> Constructor의 매개변수에 Access Modifiers 직접 적용
+*/
