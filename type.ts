c6f1d64e48694ffb7fb36a8dfadd78e7f6e8bae4
YUHASUNG <yuha822@naver.com>
2023-04-03 16:35:04 +0900

    dosc: type

--- type.ts
+++ type.ts
@@ -65,3 +65,20 @@ function saveStuentDetails(student: Student): void{
 // 아무 문제 없이 컴파일
 // 위에 작성한 student1 
 saveStuentDetails(student1);
+
+/*
+Type	JS	TS	Description
+boolean	◯	◯	true와 false
+null	◯	◯	값이 없다는 것을 명시
+undefined	◯	◯	값을 할당하지 않은 변수의 초기값
+number	◯	◯	숫자(정수와 실수, Infinity, NaN)
+string	◯	◯	문자열
+symbol	◯	◯	고유하고 수정 불가능한 데이터 타입이며 주로 객체 프로퍼티들의 식별자로 사용(ES6에서 추가)
+object	◯	◯	객체형(참조형)
+array	 	◯	배열
+tuple	 	◯	고정된 요소수 만큼의 타입을 미리 선언후 배열을 표현
+enum	 	◯	열거형. 숫자값 집합에 이름을 지정한 것이다.
+any	 	◯	타입 추론(type inference)할 수 없거나 타입 체크가 필요없는 변수에 사용. var 키워드로 선언한 변수와 같이 어떤 타입의 값이라도 할당 가능.
+void	 	◯	일반적으로 함수에서 반환값이 없을 경우 사용한다.
+never	 	◯	결코 발생하지 않는 값
+*/
