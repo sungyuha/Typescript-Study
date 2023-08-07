5d01c8a213f074a0b7764d9e6031618b480d2a67
YUHASUNG <yuha822@naver.com>
2023-08-07 19:10:43 +0900

    Dosc: 타입스크립트 컴파일

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+/*
+typescript 변수 타입 종류
+number
+string
+boolean
+null
+undefined
+any
+*/
+let a = 3;
+let b = "like you";
+// 프로젝트에서 any 타입 사용은 자제해야 함 -> 잘 안씀
+let c = 4;
+c = "abcdefg";
+// 타입 2개 지정 가능
+let d = "add";
+d = 3;
+// 배열(array) 타입
+let e = ['apple', 'momgo'];
+let f = [1, 2];
+// 함수 예제
+function addNumber(g, h) {
+    return g + h;
+}
+addNumber(3, 7);
+//# sourceMappingURL=index.js.map
