5082456387514477e7151436f2394f8bb7d766ae
YUHASUNG <yuha822@naver.com>
2023-09-29 16:20:09 +0900

    dosc: Sidebar

--- twitter-clone/components/layout/Sidebar.tsx
+++ twitter-clone/components/layout/Sidebar.tsx
@@ -1,7 +1,33 @@
+import {BsHouseFill, BsBellFill} from 'react-icons/bs';
+import {FaUser} from 'react-icons/fa';
+import SidebarLogo from './SidebarLogo';
+
 const Sidebar = () => {
+    const items = [
+        // 항목이라는 상수. 객채의 배열
+        {
+            label:'Home',
+            href: '/',
+            icon: BsHouseFill
+        },
+        {
+            label: 'Notificaticons',
+            href: '/notifications',
+            icon: BsBellFill
+        },
+        {
+            label: 'Profile',
+            href: '/user/123',
+            icon: FaUser
+        }
+    ]
     return (
-        <div>
-            
+        <div className='col-span-1 h-full pr-4 md:pr-6'>
+            <div className='flex flex-col items-end'>
+                <div className='space-y-2 lg:w-[230px]'>
+                    <SidebarLogo />
+                </div>
+            </div>
         </div>
     );
 }
