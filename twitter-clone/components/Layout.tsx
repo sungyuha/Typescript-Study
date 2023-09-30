08e7ce147c150153a35fe2334ca4d59d7c9c3f14
YUHASUNG <yuha822@naver.com>
2023-09-30 22:18:13 +0900

    dosc: TWITTER clone - 아이콘

--- twitter-clone/components/Layout.tsx
+++ twitter-clone/components/Layout.tsx
@@ -11,13 +11,13 @@ const Layout:React.FC<LayoutProps> = ({children}) => { // 함수 컴포넌트 �
     return (
         <div className='h-screen bg-black'>
             <div className='container h-full mx-auto xl:px-30 max-w-6xl'>
-                <div className='grid gird-cols-4 h-full'>
+                <div className='grid grid-cols-4 h-full'>
                     {/* 사이드바 추가 */}
                     <Sidebar />
                     <div className='
-                        col-span-3
-                        lg:col-span-2
-                        border-x-[1px]
+                        col-span-3 
+                        lg:col-span-2 
+                        border-x-[1px] 
                         border-neutral-800
                     '>
                         {/* children을 넣어줌 */}
--- twitter-clone/components/layout/SidebarLogo.tsx
+++ twitter-clone/components/layout/SidebarLogo.tsx
@@ -1,7 +1,26 @@
-const SidebarLogo = () => {
-    return (
-        <div>
+import { useRouter } from 'next/router';
+import { BsTwitter } from 'react-icons/bs';
 
+const SidebarLogo = () => {
+    const router = useRouter();
+    return (
+        <div
+        // 온클릭으로 클릭 이벤트 발생 시 경로 설정
+        onClick={() => router.push('/')}
+        className='
+            rounded-full
+            h-14
+            w-14
+            p-4 
+            flex 
+            items-center 
+            justify-center 
+            hover:bg-blue-300 
+            hover:bg-opacity-10 
+            cursor-pointer
+            transition
+        '>
+            <BsTwitter size={28} color='white' />
         </div>
     );
 }
